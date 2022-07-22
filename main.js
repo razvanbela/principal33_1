d7dcf57cb8d2d098239ffd4a44e34b86c00932ae
razvanbela <razvanvirgil.bela@ulbsibiu.ro>
2022-07-22 12:49:42 +0300

    commit

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-function alertDay(value){
-   alert("Day: " + value);
+const date = new Date();
+
+const monthDays=document.querySelector('.day')
+
+const lastDay=new Date(date.getFullYear(),date.getMonth(),1);
+
+console.log(lastDay);
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+document.querySelector(".date h2").innerHTML =
+    months[date.getMonth()];
+let days="";
+
+for (let i=1;i<=31;i++){
+    days += '<td>${i}</td>';
 }
