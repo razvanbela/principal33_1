2ee117e0e7debdaafb0a133c22b84c705e548962
razvanbela <razvanvirgil.bela@ulbsibiu.ro>
2022-07-26 13:09:48 +0300

    commit

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
 const date = new Date();
 
-const monthDays=document.querySelector('.day')
+const readerCalendar = () => date.setDate(1);
 
-const lastDay=new Date(date.getFullYear(),date.getMonth(),1);
+const monthDays = document.querySelector(".days");
 
-console.log(lastDay);
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-document.querySelector(".date h2").innerHTML =
-    months[date.getMonth()];
-let days="";
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
 
-for (let i=1;i<=31;i++){
-    days += '<td>${i}</td>';
+const firstDayIndex = date.getDay();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+
+const nextDays = 7 - lastDayIndex - 1;
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+let days = "";
+
+for (let i = firstDayIndex; i > 0; i--) {
+    days += '<div class="prev-date">${prevLastDay - i +1}</div>';
+
 }
+for (let j = 1; j <= lastDay; j++) {
+    if (j == new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+        days += '<div class="today">${j}</div>';
+    } else {
+        days += '<div>${j}</div>';
+    }
+
+}
+for (let k = 1; k <= nextDays; k++) {
+    days += '<div class="next-date">${k}</div>';
+    // monthDays.innerHTML=days;
+}
+
+document.querySelector(".prev").addEventListener('click', () => {
+    date.setMonth(date.getMonth() - 1);
+    readerCalendar();
+});
+document.querySelector(".next").addEventListener('click', () => {
+    date.setMonth(date.getMonth() + 1);
+    readerCalendar();
+});
+readerCalendar();
+
+document.querySelectorAll('.days').forEach(item => {
+    item.addEventListener('click', event => {
+        alert(event.target.innerText)
+    })
+})
